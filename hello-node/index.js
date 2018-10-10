79320e5c86e80a7fce86ac72240ba2ebec045de7
Paul Roper <paul.roper@red-badger.com>
2018-10-10 10:54:24 +0100

    Use go-http service to uppercase hello world message

--- hello-node/index.js
+++ hello-node/index.js
@@ -1,13 +1,38 @@
 const express = require('express');
+const fetch = require('node-fetch');
+
 const app = express();
 const port = process.env.PORT || 8080;
 
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {
   console.log('Received a request!');
   console.log(req);
   console.log();
 
-  return res.json({ message: 'Hello world!' });
+  let message = 'Hello world!';
+
+  try {
+    const fetchRes = await fetch('http://go-http:3000/uppercase', {
+      body: JSON.stringify({ input: message }),
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      method: 'POST',
+    });
+
+    const jsonRes = await fetchRes.json();
+    console.log(jsonRes);
+
+    message = jsonRes.output;
+  } catch (e) {
+    console.log();
+    console.log('Error talking to go-http service');
+    console.log(e);
+    console.log();
+  }
+
+  return res.json({ message });
 });
 
 app.listen(port, () => console.log(`Listening at http://localhost:${port}`));
