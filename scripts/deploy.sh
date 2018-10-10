437326ac82e70608a150515389b97d4efc0d09cd
Paul Roper <paul.roper@red-badger.com>
2018-10-10 10:30:51 +0100

    Inject Linkerd config before applying service config

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,29 +3,22 @@ set -e
 
 export $(cat .env | xargs)
 
-# elixir-magic build steps
-cd ./elixir-magic && docker build . -t $ELIXIR_MAGIC_SERVICE_NAME:$ELIXIR_MAGIC_VERSION && cd ..
-kubetpl render ./config/elixir-magic.yml -i ./.env -x $ | kubectl apply -f -
-
-# go-http build steps
-cd ./go-http && docker build . -t $GO_HTTP_SERVICE_NAME:$GO_HTTP_VERSION && cd ..
-kubetpl render ./config/go-http.yml -i ./.env -x $ | kubectl apply -f -
-
-# hello-node build steps
-cd ./hello-node && yarn build -t $HELLO_NODE_SERVICE_NAME:$HELLO_NODE_VERSION && cd ..
-kubetpl render ./config/hello-node.yml -i ./.env -x $ | kubectl apply -f -
-
-# proxy build steps
-cd ./proxy && yarn build -t $PROXY_SERVICE_NAME:$PROXY_VERSION && cd ..
-kubetpl render ./config/proxy.yml -i ./.env -x $ | kubectl apply -f -
-
 # Install linkerd
-# kubectl create ns linkerd 2> /dev/null || true
-# kubectl apply -f https://raw.githubusercontent.com/linkerd/linkerd-examples/master/k8s-daemonset/k8s/servicemesh.yml
-
 linkerd install | kubectl apply -f -
 kubectl apply -f ./config/servicemesh.yml
 
-kubectl get -n default deploy -o yaml \
-  | linkerd inject - \
-  | kubectl apply -f -
+# elixir-magic build steps
+cd ./elixir-magic && docker build . -t $ELIXIR_MAGIC_SERVICE_NAME:$ELIXIR_MAGIC_VERSION && cd ..
+kubetpl render ./config/elixir-magic.yml -i ./.env -x $ | linkerd inject - | kubectl apply -f -
+
+# go-http build steps
+cd ./go-http && docker build . -t $GO_HTTP_SERVICE_NAME:$GO_HTTP_VERSION && cd ..
+kubetpl render ./config/go-http.yml -i ./.env -x $ | linkerd inject - | kubectl apply -f -
+
+# hello-node build steps
+cd ./hello-node && yarn build -t $HELLO_NODE_SERVICE_NAME:$HELLO_NODE_VERSION && cd ..
+kubetpl render ./config/hello-node.yml -i ./.env -x $ | linkerd inject - | kubectl apply -f -
+
+# proxy build steps
+cd ./proxy && yarn build -t $PROXY_SERVICE_NAME:$PROXY_VERSION && cd ..
+kubetpl render ./config/proxy.yml -i ./.env -x $ | linkerd inject - | kubectl apply -f -
